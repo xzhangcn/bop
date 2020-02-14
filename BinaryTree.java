b3d17393b22ed767a262c92b5d707e8c14a44e13
xzhangcn <xzhangcn@hotmail.com>
2020-02-14 15:34:12 +0800

    add maxDepth and isSymmetric methods to BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,8 @@
 import edu.princeton.cs.algs4.LinkedQueue;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+
 /******************************************************************************
  *  author:     Xiaoyu Zhang
  *  created:    2020/02/13
@@ -8,7 +10,7 @@ import edu.princeton.cs.algs4.StdOut;
  *  Execution:    java-algs4 BinaryTree
  *  Dependencies: StdOut.java
  *
- *  This class is the collection of classic algorithms for binary tree.
+ *  This generic class is the collection of classic algorithms for binary tree.
  *  I purposely group together to cement my understanding of tree and recursion.
  *
  ******************************************************************************/
@@ -16,7 +18,6 @@ import edu.princeton.cs.algs4.StdOut;
 public class BinaryTree<Item> {
 
     public BinaryTree() {
-
     }
 
     public LinkedQueue<Item> preOrderTraversal(TreeNode<Item> root) {
@@ -98,6 +99,88 @@ public class BinaryTree<Item> {
         return result;
     }
 
+    // Find the depth of a binary tree with bottom-up approach
+    public int maxDepth(TreeNode<Item> root) {
+        if (root == null)   // base case
+            return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    // return true if left subtree is a mirror reflection of right subtree.
+    public boolean isSymmetric(TreeNode<Item> root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode<Item> t1, TreeNode<Item> t2) {
+        if (t1 == null && t2 == null)
+            return true;
+        if (t1 == null || t2 == null)
+            return false;
+
+        return (t1.val.equals(t2.val))
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
+    }
+
+    // Iterative version with the aid of a queue.
+    // It works similarly to BFS, with certain differences.
+    public boolean isSymmetric2(TreeNode<Item> root) {
+        LinkedQueue<TreeNode<Item>> queue = new LinkedQueue<>();
+        queue.enqueue(root);
+        queue.enqueue(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode<Item> t1 = queue.dequeue();
+            TreeNode<Item> t2 = queue.dequeue();
+            if (t1 == null && t2 == null)
+                continue;
+            if (t1 == null || t2 == null)
+                return false;
+            if (!t1.val.equals(t2.val))
+                return false;
+            queue.enqueue(t1.left);
+            queue.enqueue(t2.right);
+            queue.enqueue(t1.right);
+            queue.enqueue(t2.left);
+        }
+
+        return true;
+    }
+
+    // another algorithm to check if a tree is symmetric.
+    // run inOrderTraversal, then go through the result list to check
+    // the values at the two ends
+    public boolean isSymmetric3(TreeNode<Item> root) {
+        LinkedQueue<Item> result = inOrderTraversal(root);
+        int size = result.size();
+        if (size % 2 == 0)
+            return false;
+
+        // As java not support generic array, so use ArrayList instead.
+        // It may be better if return an ArrayList from inOrderTraversal,
+        // such that there is not need to copy elements to an ArrayList here.
+        ArrayList<Item> al = new ArrayList<>();
+        for (Item item : result) {
+            al.add(item);
+            // StdOut.println((Object) item);
+        }
+
+        for (int i = 0; i < size / 2; i++)
+            if (!al.get(i).equals(al.get(size - 1 - i)))
+                return false;
+
+        return true;
+    }
+
+    /**
+     * Unit tests the {@code FileManager} data type.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         TreeNode<Character> nF = new TreeNode<Character>('F');
         TreeNode<Character> nB = new TreeNode<Character>('B');
@@ -136,5 +219,20 @@ public class BinaryTree<Item> {
                 StdOut.printf("%c \t", c);
             StdOut.println();
         }
+
+        StdOut.printf("The depth of the tree is %d \n", bt.maxDepth(nF));
+
+        StdOut.printf("The tree is symmetric? %b \n", bt.isSymmetric(nF));
+
+        TreeNode<Integer> n1 = new TreeNode<Integer>(1);
+        TreeNode<Integer> n2_1 = new TreeNode<Integer>(2);
+        TreeNode<Integer> n2_2 = new TreeNode<Integer>(2);
+        n1.left = n2_1;
+        n1.right = n2_2;
+        BinaryTree<Integer> bt2 = new BinaryTree<>();
+        StdOut.printf("The tree is symmetric? %b \n", bt2.isSymmetric(n1));
+        StdOut.printf("The tree is symmetric? %b \n", bt2.isSymmetric2(n1));
+        StdOut.printf("The tree is symmetric? %b \n", bt2.isSymmetric3(n1));
+
     }
 }

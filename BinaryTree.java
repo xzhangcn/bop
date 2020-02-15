d1422bcbd5c07628de5edae7671d35490429462c
xzhangcn <xzhangcn@hotmail.com>
2020-02-15 08:14:20 +0800

    add hasPathSum method (both recursive and iterative approaches) to BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.LinkedQueue;
+import edu.princeton.cs.algs4.LinkedStack;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
@@ -176,6 +177,49 @@ public class BinaryTree<Item> {
         return true;
     }
 
+    // Given a binary tree and a sum, determine if the tree has a root-to-leaf path
+    // such that adding up all the values along the path equals the given sum.
+    public boolean hasPathSum(TreeNode<Integer> root, int sum) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null)
+            return (root.val == sum);
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    // iterative version
+    // With the aid of two stacks keeping track of nodes and values, this
+    // algorithm is easier to understand compared to other iterative approaches.
+    public boolean hasPathSum2(TreeNode<Integer> root, int sum) {
+        if (root == null)
+            return false;
+        LinkedStack<TreeNode<Integer>> nodeStack = new LinkedStack<>();
+        LinkedStack<Integer> valueStack = new LinkedStack<>();
+
+        nodeStack.push(root);
+        valueStack.push(root.val);
+        while (!nodeStack.isEmpty()) {
+            TreeNode<Integer> node = nodeStack.pop();
+            int value = valueStack.pop();
+            if (node.left == null && node.right == null && value == sum)
+                return true;
+            else {
+                if (node.left != null) {
+                    nodeStack.push(node.left);
+                    valueStack.push(node.left.val + value);
+                }
+
+                if (node.right != null) {
+                    nodeStack.push(node.right);
+                    valueStack.push(node.right.val + value);
+                }
+            }
+        }
+
+        return false;
+    }
+
     /**
      * Unit tests the {@code FileManager} data type.
      *
@@ -201,27 +245,33 @@ public class BinaryTree<Item> {
         nD.right = nE;
         nI.left = nH;
 
+        StdOut.println("\n Testing: preOrderTraversal");
         BinaryTree<Character> bt = new BinaryTree<>();
         for (Character c : bt.preOrderTraversal(nF))
             StdOut.printf("%c \t", c);
         StdOut.println();
 
+        StdOut.println("\n Testing: inOrderTraversal");
         for (Character c : bt.inOrderTraversal(nF))
             StdOut.printf("%c \t", c);
         StdOut.println();
 
+        StdOut.println("\n Testing: postOrderTraversal");
         for (Character c : bt.postOrderTraversal(nF))
             StdOut.printf("%c \t", c);
         StdOut.println();
 
+        StdOut.println("\n Testing: levelOrderTraversal");
         for (LinkedQueue<Character> list : bt.levelOrderTraversal(nF)) {
             for (Character c : list)
                 StdOut.printf("%c \t", c);
             StdOut.println();
         }
 
+        StdOut.println("\n Testing: maxDepth");
         StdOut.printf("The depth of the tree is %d \n", bt.maxDepth(nF));
 
+        StdOut.println("\n Testing: isSymmetric");
         StdOut.printf("The tree is symmetric? %b \n", bt.isSymmetric(nF));
 
         TreeNode<Integer> n1 = new TreeNode<Integer>(1);
@@ -234,5 +284,30 @@ public class BinaryTree<Item> {
         StdOut.printf("The tree is symmetric? %b \n", bt2.isSymmetric2(n1));
         StdOut.printf("The tree is symmetric? %b \n", bt2.isSymmetric3(n1));
 
+        StdOut.println("\n Testing: hasPathSum");
+        TreeNode<Integer> n5 = new TreeNode<Integer>(5);
+        TreeNode<Integer> n4 = new TreeNode<Integer>(4);
+        TreeNode<Integer> n8 = new TreeNode<Integer>(8);
+        TreeNode<Integer> n11 = new TreeNode<Integer>(11);
+        TreeNode<Integer> n13 = new TreeNode<Integer>(13);
+        TreeNode<Integer> n4_2 = new TreeNode<Integer>(4);
+        TreeNode<Integer> n7 = new TreeNode<Integer>(7);
+        TreeNode<Integer> n2 = new TreeNode<Integer>(2);
+        TreeNode<Integer> n1_3 = new TreeNode<Integer>(1);
+        n5.left = n4;
+        n5.right = n8;
+        n4.left = n11;
+        n8.left = n13;
+        n8.right = n4_2;
+        n11.left = n7;
+        n11.right = n2;
+        n4.right = n1_3;
+
+        int sum = 22;
+
+        StdOut.printf("recurisve approach: The tree has a path which sums up to %d? %b \n", sum,
+                      bt2.hasPathSum(n5, sum));
+        StdOut.printf("iterative approach: The tree has a path which sums up to %d? %b \n", sum,
+                      bt2.hasPathSum2(n5, sum));
     }
 }

9cc9fed986d1f9e19cb79d827dc7c12626f1f5fc
xzhangcn <xzhangcn@hotmail.com>
2020-03-08 16:13:29 +0800

    define the tree node

---
+++ algorithms/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * created:    2020/03/08
+ * <p>
+ * The {@code TreeNode} class defines a tree node.
+ *
+ * @author Xiaoyu Zhang
+ */
+
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}

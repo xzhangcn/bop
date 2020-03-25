02452bfe1acd369d6249078fd0d03103a1509d2e
xzhangcn <xzhangcn@hotmail.com>
2020-03-25 22:29:44 +0800

    Define the linked list node

---
+++ leet/ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * created:    2020/03/25
+ * <p>
+ * List Node definition.
+ *
+ * @author Xiaoyu Zhang
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}

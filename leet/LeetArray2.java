23fe0b67d45b679f68220f0354bd38724d6b1bc1
xzhangcn <xzhangcn@hotmail.com>
2020-04-10 21:31:49 +0800

    Given a set of distinct integers, nums, return all possible subsets (the power set)

--- leet/LeetArray2.java
+++ leet/LeetArray2.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * created:    2020/04/09
  * <p>
@@ -54,6 +57,40 @@ public class LeetArray2 {
     }
 
     /**
+     * Problem 2: Subsets.
+     * <p>
+     * Given a set of distinct integers, nums, return all possible subsets (the power set).
+     *
+     * Note: The solution set must not contain duplicate subsets.
+     *
+     * @param nums an array of distinct integers
+     * @return all possible subsets
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        // Algorithm: start out with an empty subset, and either take or don't take the next element in the input array.
+
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        for (int num : nums) {
+
+            // one trick here, the variable size. It's usually a good practice to cache method call results,
+            // but now it is cached for a different reason: because it changes in every iteration.
+            // If we don't want to end up with an infinite loop, we have to remember how many results were available
+            // in the previous iteration, which is exactly the size() of the result at the beginning of the current iteration.
+            int size = result.size();
+
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * Unit tests
      *
      * @param args command line arguments.
@@ -65,5 +102,17 @@ public class LeetArray2 {
         // int[] p1_height = {1,8,6,2,5,4,8,3,7};
         int[] p1_height = {3, 9, 3, 4, 7, 2, 12, 6};
         System.out.printf("The max area is %d\n", leetArray2.maxArea(p1_height));
+
+        System.out.println("\n>>> Problem 2: Subsets.");
+        // int[] p2_nums = {1, 2, 3};
+        int[] p2_nums = {2, 1, 3};
+        List<List<Integer>> p2_result = leetArray2.subsets(p2_nums);
+        for (List<Integer> list : p2_result) {
+            for (int num : list)
+                System.out.printf("%d\t", num);
+
+            System.out.println();
+        }
+
     }
 }

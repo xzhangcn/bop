669bd3a4f673755dcfb1c24d1a8ed69c45de8a21
xzhangcn <xzhangcn@hotmail.com>
2020-04-15 21:33:26 +0800

    solved the problem on 04/15 - Product of Array Except Self

--- leet/Leet30April.java
+++ leet/Leet30April.java
@@ -741,6 +741,70 @@ public class Leet30April {
     }
 
     /**
+     * Problem on 04/15/2020: Product of Array Except Self.
+     * <p>
+     * Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to
+     * the product of all the elements of nums except nums[i].
+     *
+     * Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array
+     * (including the whole array) fits in a 32 bit integer.
+     *
+     * Note: Please solve it without division and in O(n).
+     *
+     * Follow up:
+     * Could you solve it with constant space complexity? (The output array does not count as extra space
+     * for the purpose of space complexity analysis.)
+     *
+     * @param nums an integer array
+     * @return an array of product of array except self
+     */
+    public int[] productExceptSelf(int[] nums) {
+        // The length of the input array
+        int length = nums.length;
+
+        // The left and right arrays as described in the algorithm
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        // Final answer array to be returned
+        int[] answer = new int[length];
+
+        // L[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so L[0] would be 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+
+            // L[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        // R[i] contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R[length - 1] would be 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+
+            // R[i + 1] already contains the product of elements to the right of 'i + 1'
+            // Simply multiplying it with nums[i + 1] would give the product of all
+            // elements to the right of index 'i'
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        // Constructing the answer array
+        for (int i = 0; i < length; i++) {
+            // For the first element, R[i] would be product except self
+            // For the last element of the array, product except self would be L[i]
+            // Else, multiple product of all elements to the left and to the right
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
+    }
+
+    /**
      * Swap the elements in an array
      *
      * @param nums   an integer array
@@ -848,5 +912,12 @@ public class Leet30April {
         String p14_s = "abc";
         int[][] p14_shift = {{0, 1}, {1, 2}};
         System.out.printf("The shifted string is '%s'\n", leet30April.stringShift(p14_s, p14_shift));
+
+        System.out.println("\n>>> Problem on 04/15/2020: Product of Array Except Self.");
+        int[] p15_nums = {1,2,3,4};
+        int[] p15_answer = leet30April.productExceptSelf(p15_nums);
+        for (int num : p15_answer)
+            System.out.printf("%d\t", num);
+        System.out.println();
     }
 }

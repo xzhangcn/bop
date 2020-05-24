66bb418781b151efff4c75fb507021cf5aee1d72
xzhangcn <xzhangcn@hotmail.com>
2020-05-24 15:55:57 +0800

    solved the problem on 05/24/2020 - Construct Binary Search Tree from Preorder Traversal

--- leet/Leet30May.java
+++ leet/Leet30May.java
@@ -885,6 +885,42 @@ public class Leet30May {
     }
 
     /**
+     * Problem on the day of 05/23/2020: Construct Binary Search Tree from Preorder Traversal.
+     * <p>
+     * Return the root node of a binary search tree that matches the given preorder traversal.
+     *
+     * (Recall that a binary search tree is a binary tree where for every node, any descendant
+     * of node.left has a value < node.val, and any descendant of node.right has a value > node.val.
+     * Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+     *
+     * It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+     *
+     * @param preorder a preorder traversal of a BST.
+     * @return the root node of a binary search tree that matches the given preorder traversal
+     */
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = null;
+
+        for (int value : preorder) {
+            root = put(root, value);
+        }
+
+        return root;
+    }
+
+    private TreeNode put(TreeNode x, int val) {
+        if (x == null)
+            return new TreeNode(val);
+
+        int cmp = val - x.val;
+        if      (cmp < 0) x.left  = put(x.left, val);
+        else if (cmp > 0) x.right = put(x.right, val);
+        else              x.val   = val;
+
+        return x;
+    }
+
+    /**
      * Unit tests
      *
      * @param args command line arguments
@@ -1022,6 +1058,13 @@ public class Leet30May {
         for (int[] row : p23_ans)
             System.out.printf("[%d, %d]\t", row[0], row[1]);
         System.out.println();
+
+        System.out.println("\n>>> Problem on 05/24/2020: Construct Binary Search Tree from Preorder Traversal.");
+        int[] p24_preorder = {8,5,1,7,10,12};
+        TreeNode p24_root = leet30May.bstFromPreorder(p24_preorder);
+        List<Integer> p24_inorder = (new LeetTree()).inOrderTraversal(p24_root);
+        System.out.println(p24_inorder);
+
 
     }
 }

d97c16aa424bb11c1a7bea313258285aa6940325
xzhangcn <xzhangcn@hotmail.com>
2020-05-28 22:13:43 +0800

    solved the problem on 05/28/2020 - Counting Bits

--- leet/Leet30May.java
+++ leet/Leet30May.java
@@ -1060,6 +1060,31 @@ public class Leet30May {
     }
 
     /**
+     * Problem on the day of 05/28/2020: Counting Bits
+     * <p>
+     * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number
+     * of 1's in their binary representation and return them as an array.
+     *
+     * @param num a non negative integer
+     * @return an array.
+     */
+    public int[] countBits(int num) {
+        int[] res = new int[num + 1];
+        res[0] = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if ((i & 1) == 0) {
+                res[i] = res[i >> 1];
+            } else {
+                res[i] = res[i - 1] + 1;
+            }
+        }
+
+        return res;
+    }
+
+
+    /**
      * Unit tests
      *
      * @param args command line arguments
@@ -1216,5 +1241,12 @@ public class Leet30May {
         int p27_N = 4;
         int[][] p27_dislikes = {{1, 2}, {1, 3}, {2, 4}};
         System.out.printf("Possible Bipartition ? %b \n", leet30May.possibleBipartition(p27_N, p27_dislikes));
+
+        System.out.println("\n>>> Problem on 05/28/2020: Counting Bits.");
+        int p28_num = 5;
+        for (int num : leet30May.countBits(p28_num))
+            System.out.printf("%d \t", num);
+        System.out.println();
+
     }
 }

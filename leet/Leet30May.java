80075c5d953ecb259ee0b99294dd3a6fe508044e
xzhangcn <xzhangcn@hotmail.com>
2020-05-29 22:20:55 +0800

    solved the problem on 05/29/2020 - Course Schedule

--- leet/Leet30May.java
+++ leet/Leet30May.java
@@ -1083,6 +1083,50 @@ public class Leet30May {
         return res;
     }
 
+    /**
+     * Problem on the day of 05/29/2020:: Course Schedule.
+     * <p>
+     * There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+     * <p>
+     * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+     * <p>
+     * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+     *
+     * @param numCourses    number of courses
+     * @param prerequisites prerequisites matrix
+     * @return the list of nodes' values
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0 || prerequisites.length == 0)
+            return true;
+
+        // Convert graph presentation from edges to in-degree of adjacent list.
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++)
+            indegree[prerequisites[i][0]]++;    // indegree - how many prerequisites are needed.
+
+        java.util.Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                queue.add(i);
+
+        // How many courses don't need prerequisites.
+        int canFinishCount = queue.size();
+        while (!queue.isEmpty()) {
+            int prerequisite = queue.remove();  // Already finished this prerequisite course.
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (prerequisites[i][1] == prerequisite) {
+                    indegree[prerequisites[i][0]]--;
+                    if (indegree[prerequisites[i][0]] == 0) {
+                        canFinishCount++;
+                        queue.add(prerequisites[i][0]);
+                    }
+                }
+            }
+        }
+
+        return (canFinishCount == numCourses);
+    }
 
     /**
      * Unit tests
@@ -1247,6 +1291,11 @@ public class Leet30May {
         for (int num : leet30May.countBits(p28_num))
             System.out.printf("%d \t", num);
         System.out.println();
+
+        System.out.println("\n>>> Problem on 05/29/2020: Course Schedule.");
+        int p2_numCourses = 2;
+        int[][] p2_prerequisites = {{1, 0}, {0, 1}};
+        System.out.printf("Can finish all courses? %b\n", leet30May.canFinish(p2_numCourses, p2_prerequisites));
 
     }
 }

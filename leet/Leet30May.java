a76239c7e315b52f1efd43c6ca0052538e2e2969
xzhangcn <xzhangcn@hotmail.com>
2020-05-31 17:15:24 +0800

    solved the problem on 05/31/2020 - Edit Distance

--- leet/Leet30May.java
+++ leet/Leet30May.java
@@ -1136,7 +1136,7 @@ public class Leet30May {
      * (Here, the distance between two points on a plane is the Euclidean distance.)
      * <p>
      * You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
-     *
+     * <p>
      * https://leetcode.com/problems/k-closest-points-to-origin/discuss/220235/Java-Three-solutions-to-this-classical-K-th-problem.
      *
      * @param points a list of points
@@ -1159,6 +1159,39 @@ public class Leet30May {
         }
 
         return res;
+    }
+
+    /**
+     * Problem on the day of 05/31/2020: Edit Distance.
+     * <p>
+     * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+     *
+     * @param word1 a string
+     * @param word2 another string
+     * @return the minimum number of operations required to convert word1 to word2.
+     */
+    public int minDistance(String word1, String word2) {
+        // dp[i][j] : minimum steps to convert i long word1 and j long word2
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        for (int i = 0; i <= word1.length(); i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= word2.length(); j++)
+            dp[0][j] = j;
+
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))     // <--
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    // dp[i-1][j-1] : replace word1(i) with word2(j), because word1(0, i-1) == word2(0, j-1);
+                    // dp[i  ][j-1] : delete word(j)
+                    // dp[i-1][j  ] : delete word(i), because word1(0, i-1) == word2(0, j)
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
     }
 
     /**
@@ -1337,5 +1370,10 @@ public class Leet30May {
             System.out.printf("[%d, %d]\t", point[0], point[1]);
         }
         System.out.println();
+
+        System.out.println("\n>>> Problem on 05/31/2020: Edit Distance.");
+        String p31_word1 = "horse", p31_word2 = "ros";
+        System.out.printf("The edit distance between '%s' and '%s' is %d\n",
+                p31_word1, p31_word2, leet30May.minDistance(p31_word1, p31_word2));
     }
 }
